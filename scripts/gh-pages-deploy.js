6f8612e4ef24a61c52b767a68965fd7a75fd9d1f
Himanshu Digari <himanshudigari@gmail.com>
2022-04-28 02:18:08 +0530

    Update gh-pages-deploy.js

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
-const execa = require("execa");
-const fs = require("fs");
+import execa from "execa";
+import fs from "fs";
+console.info({execa}, {fs});
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
